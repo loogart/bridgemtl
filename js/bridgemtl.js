2fd634443d1e44028c5ea033883804f2b06120fc
Chris Soueidan <chris.soueidan@gmail.com>
2020-08-16 14:56:02 -0400

    homepage progress
    - bugs footer design
    - homepage hero
    - homepage sidekick
    - separator wide image

---
+++ js/bridgemtl.js
@@ -0,0 +1,16 @@
+// parallax effects
+var rellax = new Rellax('.rellax', {
+    speed: -1.2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+
+var rellax = new Rellax('.rellax-2', {
+    speed: 1.2,
+});
+
+// animated in effects
+AOS.init();
